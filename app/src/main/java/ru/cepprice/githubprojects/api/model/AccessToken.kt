350d1d61519c10913c6e0886035f2a487a8477aa
cepprice <super.cepprice@gmail.com>
2020-11-02 23:59:07 +0300

    Add AccessToken data class

---
+++ app/src/main/java/ru/cepprice/githubprojects/api/model/AccessToken.kt
@@ -0,0 +1,8 @@
+package ru.cepprice.githubprojects.api.model
+
+import com.google.gson.annotations.SerializedName
+
+data class AccessToken(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("token_type") val tokenType: String
+)
