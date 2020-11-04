f6c464f19fbb4f7438d2ebc9e1a366e4994f1fc4
cepprice <super.cepprice@gmail.com>
2020-11-05 00:01:55 +0300

    Change package in import

--- app/src/main/java/ru/cepprice/githubprojects/ui/extensions/Navigation.kt
+++ app/src/main/java/ru/cepprice/githubprojects/ui/extensions/Navigation.kt
@@ -1,7 +1,7 @@
 package ru.cepprice.githubprojects.ui.extensions
 
 import androidx.navigation.NavController
-import ru.cepprice.githubprojects.ui.fragment.AuthFragmentDirections
+import ru.cepprice.githubprojects.ui.fragment.auth.AuthFragmentDirections
 
 fun NavController.navigateToReposListFragment(accessToken: String) {
     this.navigate(AuthFragmentDirections.actionAuthFragmentToReposListFragment(accessToken))
