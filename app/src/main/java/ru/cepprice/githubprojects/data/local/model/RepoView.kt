f92f48141ec238f40a684958d72bfa72e6c3d56c
cepprice <super.cepprice@gmail.com>
2020-11-07 17:52:28 +0300

    Refactor for data binding

--- app/src/main/java/ru/cepprice/githubprojects/data/local/model/RepoView.kt
+++ app/src/main/java/ru/cepprice/githubprojects/data/local/model/RepoView.kt
@@ -1,16 +1,19 @@
 package ru.cepprice.githubprojects.data.local.model
 
 
-data class RepoView(
+data class RepoView private constructor(
     val name: String,
     val isFork: Boolean,
     val isPrivate: Boolean,
-    val branchesUrl: String,
-    val tagsUrl: String,
-    val commitsUrl: String,
-    val watchersCount: String,
-    val starsCount: String,
-    val forksCount: String
+    val branchesCount: Int,
+    val branchCaption: String,
+    val tagsCount: Int,
+    val tagCaption: String,
+    val commitsCount: Int,
+    val commitCaption: String,
+    val watchersCount: Int,
+    val starsCount: Int,
+    val forksCount: Int,
 ) {
 
     class Builder() {
@@ -18,12 +21,19 @@ data class RepoView(
         private lateinit var name: String
         private var isFork: Boolean = false
         private var isPrivate: Boolean = false
-        private lateinit var branchesUrl: String
-        private lateinit var tagsUrl: String
-        private lateinit var commitsUrl: String
-        private lateinit var watchersCount: String
-        private lateinit var starsCount: String
-        private lateinit var forksCount: String
+
+        private var branchesCount: Int = 1
+        private var branchCaption: String = "branch"
+
+        private var tagsCount: Int = 0
+        private var tagCaption: String = "tags"
+
+        private var commitsCount: Int = 0
+        private var commitCaption: String = "commits"
+
+        private var watchersCount: Int = 1
+        private var starsCount: Int = 0
+        private var forksCount: Int = 0
 
 
         fun name(name: String) = apply { this.name = name }
@@ -32,20 +42,25 @@ data class RepoView(
 
         fun isPrivate(isPrivate: Boolean) = apply { this.isPrivate = isPrivate }
 
-        fun branchesUrl(branchesUrl: String) = apply { this.branchesUrl = branchesUrl }
+        fun branchesCount(branchesCount: Int) = apply { this.branchesCount = branchesCount }
 
-        fun tagsUrl(tagsUrl: String) = apply { this.tagsUrl = tagsUrl }
+        fun tagsCount(tagsCount: Int) = apply { this.tagsCount = tagsCount }
 
-        fun commitsUrl(commitsUrl: String) = apply { this.commitsUrl = commitsUrl }
+        fun commitsCount(commitsCount: Int) = apply { this.commitsCount = commitsCount }
 
-        fun watchersCount(watchersCount: String) = apply { this.watchersCount = watchersCount }
+        fun watchersCount(watchersCount: Int) = apply { this.watchersCount = watchersCount }
 
-        fun starsCount(starsCount: String) = apply { this.starsCount = starsCount }
+        fun starsCount(starsCount: Int) = apply { this.starsCount = starsCount }
 
-        fun forksCount(forksCount: String) = apply { this.forksCount = forksCount}
+        fun forksCount(forksCount: Int) = apply { this.forksCount = forksCount}
 
-        fun build()
-                = RepoView(name, isFork, isPrivate, branchesUrl, tagsUrl, commitsUrl,
-            watchersCount, starsCount, forksCount)
+        fun build(): RepoView {
+            val branchCaption = if (branchesCount == 1) "branch" else "branches"
+            val tagCaption = if (tagsCount == 1) "tag" else "tags"
+            val commitCaption = if (commitsCount == 1) "commit" else "commits"
+
+            return RepoView(name, isFork, isPrivate, branchesCount, branchCaption, tagsCount,
+                tagCaption, commitsCount, commitCaption, watchersCount, starsCount, forksCount)
+        }
     }
 }
