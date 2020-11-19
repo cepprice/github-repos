84af5c42fc765421ad595de40fa41d272cddb4bd
cepprice <super.cepprice@gmail.com>
2020-11-19 14:39:39 +0300

    Remove unused files

--- app/src/main/java/ru/cepprice/githubprojects/data/remote/model/license/License.kt
+++
@@ -1,9 +0,0 @@
-package ru.cepprice.githubprojects.data.remote.model.license
-
-data class License(
-    val key: String,
-    val name: String,
-    val node_id: String,
-    val spdx_id: String,
-    val url: String
-)
